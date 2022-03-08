586b985ddbfd8c24b42e72fcbd2e9406ef593427
DongkunXu <91888968+DongkunXu@users.noreply.github.com>
2022-03-08 00:30:58 +0000

    Deploying to gh-pages from @ DongkunXu/2021_STUDENT20281089@cbb23eea8de0d49bc24a85daac9b1f506532587b 🚀

--- search/files_0.js
+++ search/files_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adder_2eh_3',['adder.h',['../adder_8h.html',1,'']]]
+  ['adder_2eh_2',['adder.h',['../adder_8h.html',1,'']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,24 +1,21 @@
 var indexSectionsWithContent =
 {
-  0: "ar",
+  0: "a",
   1: "a",
-  2: "a",
-  3: "r"
+  2: "a"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "files",
-  2: "functions",
-  3: "pages"
+  2: "functions"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Files",
-  2: "Functions",
-  3: "Pages"
+  2: "Functions"
 };
 
